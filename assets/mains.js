fddbd1567e56f59ae0d0f43c4cb336d1b91adb96
Soufian <soufian.souilm@gmail.com>
2023-10-19 21:06:19 +0200

    Aufgabe JS

---
+++ assets/mains.js
@@ -0,0 +1,19 @@
+function returnOne() {
+  return 1;
+}
+
+let x = 1;
+let y = returnOne();
+
+if (x === y) {
+  console.log("Wird das gedruckt?");
+}
+
+function soufian(){
+soufian 2;
+}
+let a=2;
+let b=soufian();
+if(a===b){
+    console.log("richtig")
+}
